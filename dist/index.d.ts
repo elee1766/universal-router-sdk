d4ab4e24f0e904c5e69af8168e2da0e17da5c770
a <a@tuxpa.in>
2023-11-07 01:24:22 -0600

    remove

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { SwapRouter } from './swapRouter';
-export * from './entities';
-export { RESOLVE_PERMIT2_ADDRESS, UNIVERSAL_ROUTER_ADDRESS, PERMIT2_ADDRESS, ROUTER_AS_RECIPIENT, WETH_ADDRESS } from './utils/constants';
