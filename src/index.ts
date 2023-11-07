7342116d85aae2fd5d968c561b735c48b10e43f0
a <a@tuxpa.in>
2023-11-07 01:28:07 -0600

    resolve

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 export { SwapRouter } from './swapRouter'
 export * from './entities'
-<<<<<<< HEAD
-export { RESOLVE_PERMIT2_ADDRESS, UNIVERSAL_ROUTER_ADDRESS, PERMIT2_ADDRESS, ROUTER_AS_RECIPIENT, WETH_ADDRESS } from './utils/constants'
-=======
 export { RoutePlanner, CommandType } from './utils/routerCommands'
 export {
   UNIVERSAL_ROUTER_ADDRESS,
@@ -10,5 +7,5 @@ export {
   PERMIT2_ADDRESS,
   ROUTER_AS_RECIPIENT,
   WETH_ADDRESS,
+  RESOLVE_PERMIT2_ADDRESS,
 } from './utils/constants'
->>>>>>> d496ba03426a6c855885e8eec92370e517c50668
